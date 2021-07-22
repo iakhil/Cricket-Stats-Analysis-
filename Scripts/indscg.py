67402c790c6162e2273c3526d23d4686403110da
I Akhil <52360071+iakhil@users.noreply.github.com>
2021-07-22 09:25:07 +0530

    Update indscg.py

--- Scripts/indscg.py
+++ Scripts/indscg.py
@@ -1,3 +1,4 @@
+# Plot of the number of balls faced by Indian batsmen in the SCG Test of 2021.
 import matplotlib.pyplot as plt 
 labels = 'Rohit', 'Gill', 'Pujara', 'Rahane', 'Pant', 'Vihari', 'Ashwin'
 balls = [98, 64, 205, 18, 118, 161, 128]
