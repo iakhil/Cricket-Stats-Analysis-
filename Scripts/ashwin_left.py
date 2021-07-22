a9cc580ba101da84fe30d9443ce505301a9e5e53
I Akhil <52360071+iakhil@users.noreply.github.com>
2021-07-22 09:26:07 +0530

    Update ashwin_left.py

--- Scripts/ashwin_left.py
+++ Scripts/ashwin_left.py
@@ -1,3 +1,4 @@
+# Plot of the number of wickets taken by Ashwin against left-handed and right-handed batsmen.
 import matplotlib.pyplot as plt 
 labels = 'Left-handed batsmen', 'Right-Handed Batsmen'
 sizes = [193, 183]
